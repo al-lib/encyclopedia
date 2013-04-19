45c86f8057e856e9d570863823ce47895a88d74a
Alexander Dubrovin <al-gitmail@ya.ru>
2013-04-19 14:08:18 +0400

    using object

--- add.php
+++ add.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <!-- http://sitear.ru/material/jquery-validaciya-form-i-polej -->
 <?php
+include("class/database_manager.class.php");
+ $db_manager=new Database_manager;
+ $db_manager->connect();
 
 ?>
 <html>
@@ -12,58 +15,59 @@
 	</head>
 
 	<body>
+		<div>
+			<?php 
+				echo $db_manager->db_state;
+			?>
+		</div>
 		<div id="content">
 		  <form id="jform" action="upload.php" method="post" enctype="multipart/form-data">
-		  	<fieldset >
+		  <fieldset>
 		  		<legend>Личность</legend>
 		  		<table>
-		<tr>
-			<td class="description_column">
-				<label for="person_name">Имя: </label>
-			</td>
-			<td>
-				<input type="text" name="person_name" id="person_name" size="40"/>
-			</td>
-		</tr>
-		<tr>
-			<td class="description_column">
-				<label for="brief">Краткая информация: </label>
-			</td>
-			<td>
-				<textarea name="brief" id="brief" cols="20" rows="3"></textarea>
-			</td>
-		</tr>
-	</table>
+					<tr>
+						<td class="description_column">
+							<label for="person_name">Имя: </label>
+						</td>
+						<td>
+							<input type="text" name="person_name" id="person_name" size="40"/>
+						</td>
+					</tr>
+					<tr>
+						<td class="description_column">
+							<label for="brief">Краткая информация: </label>
+						</td>
+						<td>
+							<textarea name="brief" id="brief" cols="20" rows="3"></textarea>
+						</td>
+					</tr>
+				</table>
 
 		  		<fieldset>
 		  		<legend>Внешние файлы</legend>
-
-		  		<table>
-
-			 <!--
-				<tr>
-					<td class="description_column">
-						<label for="photo">Фотография </label>
-					</td>
-					<td>
-						<input type="file" accept="image/*" multiple name="photo" id="photo"/>
-					</td>
-				</tr>
-			-->
-				<tr>
-					<td class="description_column">
-							<label for="article">Статья</label>
-					</td>
-					<td>
-							<input type="file" accept="text/html"  name="article" id="article"/>
-					</td>
-				</tr>
-			</table>
-
+				  	<table>
+				 <!--
+						<tr>
+							<td class="description_column">
+								<label for="photo">Фотография </label>
+							</td>
+							<td>
+								<input type="file" accept="image/*" multiple name="photo" id="photo"/>
+							</td>
+						</tr>
+					-->
+						<tr>
+							<td class="description_column">
+									<label for="article">Статья</label>
+							</td>
+							<td>
+									<input type="file" accept="text/html"  name="article" id="article"/>
+							</td>
+						</tr>
+					</table>
 			  	</fieldset>
 			  	<fieldset>
 			  		<legend>Параметры поиска </legend>
-
 			  		<table>
 						<tr>
 							<td class="description_column">
@@ -71,7 +75,9 @@
 							</td>
 							<td>
 								<select name="action" id="action">
-			  						<option value="1">1</option>
+			  					<?php
+			  					$db_manager->get_option_list("action");
+			  					?>
 			  					</select>
 							</td>
 						</tr>
@@ -83,6 +89,9 @@
 								<select name="age" id="age">
 				  					<option value="1">1</option>
 				  					<option value="2">2</option>
+				  					<?php
+			  						//	$db_manager->get_option_list("age");
+			  						?>
 			  					</select>
 							</td>
 						</tr>
@@ -96,50 +105,15 @@
 						</tr>
 					</table>
 			  	</fieldset>
-			  	<fieldset>
-				  		<legend>Резерв</legend>
-				</fieldset>
+	
 				  	<p align="center">
 				  		<input type="submit" id='send' value="Добавить"/> <input type="reset" value="Сбросить"/>
 				  	</p>
 		  	</fieldset>
-
-
-
-
-
 		  </form>
-
 		</div>
 
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" charset="utf-8"></script>
 		<script type="text/javascript" src="scripts/validate.js" charset="utf-8"></script>
-
-	<table>
-		<tr>
-			<td>
-
-			</td>
-			<td>
-
-			</td>
-		</tr>
-		<tr>
-			<td>
-
-			</td>
-			<td>
-
-			</td>
-		</tr>
-		<tr>
-			<td>
-
-			</td>
-			<td>
-
-			</td>
-		</tr>
-	</table>
 	</body>
 </html>
