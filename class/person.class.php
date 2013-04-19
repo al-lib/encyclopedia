a05a636f883199105caa00c3842686aadcb2aff4
Alexander Dubrovin <al-gitmail@ya.ru>
2013-04-19 14:54:12 +0400

    announced all functions

--- class/person.class.php
+++ class/person.class.php
@@ -1,8 +1,16 @@
 <?php
     class Person{
     	
-		function check_name($name){}
-		function file_safety($path_to_file){}
-		
-    }
+		public function check_name($name_value){
+			$name_value=trim($name_value);
+		}
+		public function file_safety($path_to_file){
+			
+		}
+		public function check_short_description($short_description_value){
+			
+		}
+		public function parse_keywords($keywords_value)	{
+		}
+   }
 ?>
